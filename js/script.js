b4c2ef8d01bf88264cabc680960c7c94869567a2
Dhilan <dhilan.patel87@gmail.com>
2018-11-06 01:52:37 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	// $("#main-nav-container").load("nav.html");
+	
+	// Proper way to load the htmls with jquery load method - from tuts+
+	// $(".page-links").on('click', function( e ) {
+	// 	var href = $(this).attr('href');
+	// 	$('.content-row').load(href + ' .content-container');
+	// 	e.preventDefault();
+	// 	/* Act on the event */
+	// });
+
+});
